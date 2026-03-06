2d4e156887098e79c5712d2e1d0e3d439f857117
kanikanoorsathi <kahinurakter504@gmail.com>
2026-03-06 16:20:28 +0600

    calender added

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,22 +1,25 @@
+import { Calendar } from 'lucide-react';
 import React from 'react';
+// import { Calender } from 'lucide-react';
 
 const Ticket = ({ticket}) => {
-    console.log(ticket)
+    //console.log(ticket)
+    const {title, description, id, priority, customer, createdAt } = ticket
     return (
-        <div className="bg-white p-8 rounded-xl shadow-sm border border-gray-100 my-6">
+        <div className="bg-white p-8 rounded-xl shadow-sm border border-gray-100 cursor-pointer hover:bg-violet-400 my-6">
             <div className='flex flex-wrap justify-between mb-3'>
-                <h3 className='font-bold text-xl text-black'>{ticket.title}</h3>
+                <h3 className='font-bold text-xl text-black'>{title}</h3>
                 <span className='badge badge-success text-white '>Open</span>
             </div>
-            <p className='font-normal text-gray-500 text-base'>{ticket.description}</p>
-            <div className='grid grid-cols-2'>
+            <p className='font-normal text-gray-500 text-base'>{description}</p>
+            <div className='grid grid-cols-2 py-4'>
                 <div className='flex justify-start'>
-                    <p className='font-normal text-gray-500 text-base mr-6'>{ticket.id}</p>
-                    <p className='font-normal text-red-600 text-sm'>{ticket.priority}</p>
+                    <p className='font-normal text-gray-500 text-base mr-6'>#{id}</p>
+                    <p className='font-normal text-red-600 text-sm'>{priority}</p>
                 </div>
-                <div className='flex flex-wrap justify-between gap-1'>
-                    <p className='font-normal text-gray-500 text-base'>{ticket.customer}</p>
-                    <p className='font-normal text-gray-500 text-base'>{ticket.createdAt}</p>
+                <div className='flex flex-wrap justify-between'>
+                    <p className='font-normal text-gray-500 text-base'>{customer}</p>
+                    <p className='font-normal text-gray-500 text-sm'><Calendar size={14}></Calendar>{createdAt}</p>
 
                 </div>
             </div>
